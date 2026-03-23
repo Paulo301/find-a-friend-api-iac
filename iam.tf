e29b29c82b40cafcdb79603cef387150ae45f2d0
Paulo Victor Lemos de Almeida <pauloalmeida@ma.def.br>
2026-03-23 12:37:00 -0300

    feat: create apprunner role and configure ecr to accept apprunner

--- iam.tf
+++ iam.tf
@@ -16,7 +16,7 @@ resource "aws_iam_role" "ecr-role" {
     Statement = [
       {
         Effect : "Allow",
-        Action : "",
+        Action : "sts:AssumeRoleWithWebIdentity",
         Principal : {
           Federated : ""
         },
@@ -43,7 +43,19 @@ resource "aws_iam_role_policy" "ecr-role-policy" {
     Version = "2012-10-17"
     Statement = [
       {
-        Sid = "Statement1"
+        Sid      = "Statement1",
+        Action   = "apprunnner:*",
+        Effect   = "Allow",
+        Resource = "*",
+      },
+      {
+        Sid      = "Statement2",
+        Action   = ["iam:PassRole", "iam:CreateServiceLinkedRole"],
+        Effect   = "Allow",
+        Resource = "*",
+      },
+      {
+        Sid = "Statement3"
         Action = [
           "ecr:GetDownloadUrlForLayer",
           "ecr:BatchGetImage",
@@ -61,4 +73,46 @@ resource "aws_iam_role_policy" "ecr-role-policy" {
   })
 
   depends_on = [aws_iam_role.ecr-role]
+}
+
+resource "aws_iam_role" "app-runner-role" {
+  name = "app-runner-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect : "Allow",
+        Action : "sts:AssumeRole",
+        Principal : {
+          Service : "build.apprunner.amazonaws.com"
+        },
+      },
+    ]
+  })
+
+  tags = {
+    IAC = true
+  }
+}
+
+data "aws_iam_policy_document" "app-runner-policy" {
+  statement {
+    effect    = "Allow"
+    actions   = ["ec2:ReadOnly"]
+    resources = ["*"]
+  }
+}
+
+resource "aws_iam_policy" "app-runner-policy" {
+  name   = "app-runner-policy"
+  policy = data.aws_iam_policy_document.app-runner-policy.json
+}
+
+
+resource "aws_iam_role_policy_attachment" "app-runner-policy-attachment" {
+  role       = aws_iam_role.app-runner-role
+  policy_arn = aws_iam_policy.app-runner-policy.arn
+
+  depends_on = [aws_iam_role.app-runner-role]
 }

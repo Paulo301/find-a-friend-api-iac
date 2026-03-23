81dfe2c227f85ef75d9db1da4a09f18909a961e1
Paulo Victor Lemos de Almeida <pauloalmeida@ma.def.br>
2026-03-23 11:43:18 -0300

    feat: configure oidc policy and create ecr

--- iam.tf
+++ iam.tf
@@ -7,3 +7,58 @@ resource "aws_iam_openid_connect_provider" "oidc-git" {
     IAC = true
   }
 }
+
+resource "aws_iam_role" "ecr-role" {
+  name = "ecr-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Effect : "Allow",
+        Action : "",
+        Principal : {
+          Federated : ""
+        },
+        Condition : {
+          StringEquals : {
+            "token:aud" : [""],
+            "token:sub" : [""]
+          }
+        }
+      },
+    ]
+  })
+
+  tags = {
+    IAC = true
+  }
+}
+
+resource "aws_iam_role_policy" "ecr-role-policy" {
+  name = "ecr-role-policy"
+  role = aws_iam_role.ecr-role.id
+
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Sid = "Statement1"
+        Action = [
+          "ecr:GetDownloadUrlForLayer",
+          "ecr:BatchGetImage",
+          "ecr:BatchCheckLayerAvailability",
+          "ecr:PutImage",
+          "ecr:InitiateLayerUpload",
+          "ecr:UploadLayerPart",
+          "ecr:CompleteLayerUpload",
+          "ecr:GetAuthorizationToken",
+        ]
+        Effect   = "Allow"
+        Resource = "*"
+      },
+    ]
+  })
+
+  depends_on = [aws_iam_role.ecr-role]
+}
